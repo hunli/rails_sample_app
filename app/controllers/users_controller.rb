24b59da6f5670f0bcaa371ad292ce0010b8475c1
Hunter <>
2017-12-14 11:44:20 -0800

    Adding signup capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -21,5 +22,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user)
           .permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def index
   end
 end
